d40024b304af87f3767ee93eb4422de47ab600ad
Willow <willow.ch@protonmail.com>
2022-09-21 10:05:31 -0500

    Made the background a little more performant

--- src/background.ts
+++ src/background.ts
@@ -1,6 +1,6 @@
 import type {GameState} from 'src';
 
-const bgTileSize = 4;
+const bgTileSize = 12;
 
 export function background(game_state: GameState, ctx: CanvasRenderingContext2D) {
 	if (window.matchMedia('(prefers-reduced-motion: reduce)').matches) {
@@ -9,8 +9,8 @@ export function background(game_state: GameState, ctx: CanvasRenderingContext2D)
 		return;
 	}
 
-	for (let x = 0; x < ctx.canvas.width; x += bgTileSize * 3) {
-		for (let y = 0; y < ctx.canvas.height; y += bgTileSize) {
+	for (let x = 0; x < ctx.canvas.width; x += 4) {
+		for (let y = 0; y < ctx.canvas.height + 20; y += bgTileSize) {
 			const timer = (game_state.clock / 5000) + (Math.sin((y / 5) + (game_state.clock / -500)) / 5) + (Math.cos((x / 5) + (game_state.clock / 100)) / 15);
 			const r = 128 + (127 * Math.sin(timer));
 			const g = 128 + (127 * Math.sin((timer) + (2 * Math.PI / 3)));
@@ -20,8 +20,8 @@ export function background(game_state: GameState, ctx: CanvasRenderingContext2D)
 				ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 			}
 
-			const offset = Math.sin((y / 20) + (timer / 10)) * 500;
-			ctx.fillRect(nicerModulo(x + offset, ctx.canvas.width + 20) - 10, y, bgTileSize * 4, bgTileSize);
+			const offset = Math.sin((y / 20) + ((game_state.clock / 5000) + (Math.sin((y / 5))))) * 500;
+			ctx.fillRect(nicerModulo(x + offset, ctx.canvas.width + 20) - 10, nicerModulo(y + (game_state.clock / 50), ctx.canvas.height + 20) - 10, 5, bgTileSize);
 		}
 	}
 }
