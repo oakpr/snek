2056d24e0e28cbbec0e67b83016e23d386f654d0
Willow <willow.ch@protonmail.com>
2022-09-29 20:45:00 -0500

    indentation wack

--- snowpack.config.js
+++ snowpack.config.js
@@ -3,21 +3,21 @@
 
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
-  mount: {
-    public: { url: '/', static: true },
-    src: { url: '/dist' },
-  },
-  plugins: [
-    '@snowpack/plugin-typescript',
-    /* ... */
-  ],
-  packageOptions: {
-    /* ... */
-  },
-  devOptions: {
-    /* ... */
-  },
-  buildOptions: {
-    /* ... */
-  },
+	mount: {
+		public: {url: '/', static: true},
+		src: {url: '/dist'},
+	},
+	plugins: [
+		'@snowpack/plugin-typescript',
+		/* ... */
+	],
+	packageOptions: {
+		/* ... */
+	},
+	devOptions: {
+		/* ... */
+	},
+	buildOptions: {
+		/* ... */
+	},
 };
