4fc58fe5883938e83166e0fc833c7ac17da7e455
Willow <willow.ch@protonmail.com>
2022-10-04 18:20:55 -0500

    fixed linterage

--- src/grid.ts
+++ src/grid.ts
@@ -1,48 +1,50 @@
 import type {GameState} from 'src';
 
 export default function grid(ctx: CanvasRenderingContext2D, gameState: GameState) {
-	const widthCells = gameState.settings["gridWidth"]; 
-	const heightCells = gameState.settings["gridHeight"]; 
+	const widthCells = gameState.settings.gridWidth as number;
+	const heightCells = gameState.settings.gridHeight as number;
 	const cellSize = cellSizeHelper(ctx, gameState);
 
 	const gridW = widthCells * cellSize;
 	const gridH = heightCells * cellSize;
 	const centerX = ctx.canvas.width / 2;
 	const centerY = ctx.canvas.height / 2;
-	
+
 	// Draw borders
 	ctx.strokeStyle = 'black';
-	ctx.lineWidth = 2
+	ctx.lineWidth = 2;
 	ctx.beginPath();
-	ctx.moveTo(centerX - (gridW / 2), centerY - (gridH / 2))
-	ctx.lineTo(centerX + (gridW / 2), centerY - (gridH / 2))
-	ctx.lineTo(centerX + (gridW / 2), centerY + (gridH / 2))
-	ctx.lineTo(centerX - (gridW / 2), centerY + (gridH / 2))
-	ctx.lineTo(centerX - (gridW / 2), centerY - (gridH / 2))
-	ctx.lineTo(centerX + (gridW / 2), centerY - (gridH / 2))
+	ctx.moveTo(centerX - (gridW / 2), centerY - (gridH / 2));
+	ctx.lineTo(centerX + (gridW / 2), centerY - (gridH / 2));
+	ctx.lineTo(centerX + (gridW / 2), centerY + (gridH / 2));
+	ctx.lineTo(centerX - (gridW / 2), centerY + (gridH / 2));
+	ctx.lineTo(centerX - (gridW / 2), centerY - (gridH / 2));
+	ctx.lineTo(centerX + (gridW / 2), centerY - (gridH / 2));
 	ctx.stroke();
 	ctx.beginPath();
 	ctx.strokeStyle = 'rgba(1.0, 1.0, 1.0, 0.5)';
-	ctx.lineWidth = 1
+	ctx.lineWidth = 1;
 
 	const left = centerX - (gridW / 2);
 	const top = centerY - (gridH / 2);
 	// Draw vertical lines
 	for (let i = 1; i < widthCells; i++) {
-		ctx.moveTo(left + (cellSize * i), top)
-		ctx.lineTo(left + (cellSize * i), top + (cellSize * heightCells))
+		ctx.moveTo(left + (cellSize * i), top);
+		ctx.lineTo(left + (cellSize * i), top + (cellSize * heightCells));
 	}
+
 	// Draw horizontal lines
 	for (let i = 1; i < heightCells; i++) {
 		ctx.moveTo(left, top + (cellSize * i));
 		ctx.lineTo(left + (cellSize * widthCells), top + (cellSize * i));
 	}
+
 	ctx.stroke();
 }
 
 export function cellSizeHelper(ctx: CanvasRenderingContext2D, gameState: GameState): number {
-	const widthCells = gameState.settings["gridWidth"]; 
-	const heightCells = gameState.settings["gridHeight"]; 
+	const widthCells = gameState.settings.gridWidth as number;
+	const heightCells = gameState.settings.gridHeight as number;
 	const fieldW = ctx.canvas.width - 32;
 	const fieldH = ctx.canvas.height - 96;
 	const cellSize = Math.min(
@@ -53,8 +55,8 @@ export function cellSizeHelper(ctx: CanvasRenderingContext2D, gameState: GameSta
 }
 
 export function cellPositionHelper(ctx: CanvasRenderingContext2D, gameState: GameState, position: [number, number], cellSizeInput: number | undefined): [number, number] {
-	const widthCells = gameState.settings["gridWidth"]; 
-	const heightCells = gameState.settings["gridHeight"]; 
+	const widthCells = gameState.settings.gridWidth as number;
+	const heightCells = gameState.settings.gridHeight as number;
 	const cellSize = cellSizeInput || cellSizeHelper(ctx, gameState);
 	const gridW = widthCells * cellSize;
 	const gridH = heightCells * cellSize;
@@ -64,7 +66,7 @@ export function cellPositionHelper(ctx: CanvasRenderingContext2D, gameState: Gam
 	const top = centerY - (gridH / 2);
 
 	return [
-		left + (cellSize * position[0] + 0.5),
-		top + (cellSize * (position[1] + 0.5))
-	]
+		left + (cellSize * (position[0] + 0.5)),
+		top + (cellSize * (position[1] + 0.5)),
+	];
 }
