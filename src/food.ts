0531cb453b8cf88cf77144c9fb3eca91eba68ce7
Willow <willow.ch@protonmail.com>
2022-10-12 11:10:39 -0500

    Tweak Ismail's food script so it at least compiles

--- src/food.ts
+++ src/food.ts
@@ -1,29 +1,30 @@
+import type {GameState} from 'src';
 
-// randomly populates fruit
-let food = getRandomFoodPosition
-const EXPANSION_RATE = 1
+// Randomly populates fruit
+let food: [number, number] | undefined;
+// This should be able to vary for different types of fruit
+const expansionRate = 1;
 
-// if snake is on food, expand the snake
-export function update() {
-    if (onSnake(food)){
-        expandSnake(EXPANSION_RATE)
-        food = getRandomFoodPosition
-		
-    }
- 
+// If snake is on food, expand the snake
+export function update(gameState: GameState) {
+	if (!food) {
+		food = getRandomFoodPosition(gameState);
+	}
+
+	// Tweaked to work with our data structures
+	for (const snake of gameState.players.map(p => p.snake)) {
+		if (snake.intersects(food)) {
+			snake.len += expansionRate;
+			food = getRandomFoodPosition(gameState);
+		}
+	}
 }
-export function draw(gameBoard) {
-    const foodElement = document.createElement('div')
-    foodElement.style.gridRowStart = food.y
-    foodElement.style.gridColumnStart = food.x
-    foodElement.classList.add('food')
-    gameBoard.appendChild(foodElement)
 
+export function draw(gameState: GameState, ctx: CanvasRenderingContext2D) {
+	// Rewrite this to support multiple fruits and draw on the canvas.
 }
-function getRandomFoodPosition(){
-    let newFoodPosition
-    while (newFoodPosition == null || onSnake(newFoodPosition)){
-        newFoodPosition = randomGridPosition()
-    }
-    return newFoodPosition
+
+function getRandomFoodPosition(gameState: GameState): [number, number] {
+	// Rewrite to read our grid settings and use the snake.intersects method
+	return [0, 0];
 }
